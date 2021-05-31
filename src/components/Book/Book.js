e6b47f8f497f850cc23a99861db4b44a42393995
IMykolaI <mykola.motychak.knm.2018@lpnu.ua>
2021-05-31 20:41:01 +0300

    Add full CRUD on frontend for Books and Users

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,21 +1,96 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Modal, Button, Form } from 'react-bootstrap'
 import './Book.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import delete_icon from '../icons/delete_black_36dp.svg';
 import edit_icon from "../icons/edit_black_36dp.svg"
+import { useDispatch } from "react-redux";
+import { fetchBooks } from "../../redux/actions";
+import { deleteBook, getBook, editBook } from "../../redux/saga"
 
 const Book = ({book}) => {
+    const dispatch = useDispatch();
+
+    const [showEdit, setShowEdit] = useState(false);
+    const [id, setId] = useState("");
+    const [name, setName] = useState(""); 
+    const [creator, setCreator] = useState("");
+    const [genre, setGenre] = useState("")
+
+    const deleteBookById = (bookId) => {
+        deleteBook(bookId).then(() => {
+            dispatch(fetchBooks())
+        })
+    }
+
+    const handleShowEdit = (bookId) => {
+        getBook(bookId).then((data) => {
+            setId(book.id);
+            setName(data.name);
+            setCreator(data.creator);
+            setGenre(data.genre);
+            setShowEdit(true)
+        })
+    };
+    const handleCloseEdit = () => setShowEdit(false);
+    const handleSubmitEdit = () => {
+        editBook(id, name, creator, genre).then(() => {
+            setShowEdit(false);
+            dispatch(fetchBooks())
+        })
+    }
+
+    const nameOnChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const creatorOnChange = (e) => {
+        setCreator(e.target.value);
+    }
+
+    const genreOnChange = (e) => {
+        setGenre(e.target.value);
+    }
+
     return (
-        <tr id={book.id}>
-            <td>{book.id}</td>
-            <td>{book.name}</td>
-            <td>{book.creator}</td>
-            <td>{book.genre}</td>
-            <td>{book.created_at}</td>
-            <td>{book.deleted_at}</td>
-            <td>{book.createdAt}</td>
-            <td>{book.updatedAt}</td>
-            <td><img src={edit_icon}/><img src={delete_icon}/></td>
-        </tr>
+        <>
+            <tr id={book.id}>
+                <td>{book.id}</td>
+                <td>{book.name}</td>
+                <td>{book.creator}</td>
+                <td>{book.genre}</td>
+                <td>{book.created_at}</td>
+                <td>{book.deleted_at}</td>
+                <td>{book.createdAt}</td>
+                <td>{book.updatedAt}</td>
+                <td><img onClick={() => handleShowEdit(book.id)} src={edit_icon}/><img onClick={() => deleteBookById(book.id)} src={delete_icon}/></td>
+            </tr>
+            <Modal show={showEdit} onHide={handleCloseEdit} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit a book</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control onChange={nameOnChange} value={name}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Creator</Form.Label>
+                            <Form.Control onChange={creatorOnChange} value={creator}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Genre</Form.Label>
+                            <Form.Control onChange={genreOnChange} value={genre}/>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseEdit}>Close</Button>
+                    <Button onClick={handleSubmitEdit}>Save</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
 
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,22 +1,107 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Modal, Button, Form } from 'react-bootstrap'
 import './User.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import delete_icon from '../icons/delete_black_36dp.svg';
 import edit_icon from "../icons/edit_black_36dp.svg"
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../../redux/actions";
+import { deleteUser, getUser, editUser } from "../../redux/saga"
 
 const User = ({user}) => {
+    const dispatch = useDispatch();
+
+    const [showEdit, setShowEdit] = useState(false);
+    const [id, setId] = useState("");
+    const [fullName, setFullName] = useState("");
+    const [address, setAddress] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [category, setCategory] = useState("");
+
+    const deleteUserById = (userId) => {
+        deleteUser(userId).then(() => {
+            dispatch(fetchUsers())
+        })
+    }
+
+    const handleShowEdit = (userId) => {
+        getUser(userId).then((data) => {
+            setId(user.id);
+            setFullName(user.full_name);
+            setAddress(user.address);
+            setPhoneNumber(user.phone_number);
+            setCategory(user.category);
+            setShowEdit(true)
+        })
+    };
+    const handleCloseEdit = () => setShowEdit(false);
+    const handleSubmitEdit = () => {
+        editUser(id, fullName, address, phoneNumber, category).then(() => {
+            setShowEdit(false);
+            dispatch(fetchUsers())
+        })
+    }
+
+    const fullNameOnChange = (e) => {
+        setFullName(e.target.value)
+    }
+
+    const addressOnChange = (e) => {
+        setAddress(e.target.value)
+    }
+
+    const phoneNumberOnChange = (e) => {
+        setPhoneNumber(e.target.value)
+    }
+
+    const categoryOnChange = (e) => {
+        setCategory(e.target.value)
+    }
+
     return (
-        <tr id={user.id}>
-            <td>{user.id}</td>
-            <td>{user.full_name}</td>
-            <td>{user.address}</td>
-            <td>{user.phone_number}</td>
-            <td>{user.category}</td>
-            <td>{user.created_at}</td>
-            <td>{user.deleted_at}</td>
-            <td>{user.createdAt}</td>
-            <td>{user.updatedAt}</td>
-            <td><img src={edit_icon}/><img src={delete_icon}/></td>
-        </tr>
+        <>
+            <tr id={user.id}>
+                <td>{user.id}</td>
+                <td>{user.full_name}</td>
+                <td>{user.address}</td>
+                <td>{user.phone_number}</td>
+                <td>{user.category}</td>
+                <td>{user.created_at}</td>
+                <td>{user.deleted_at}</td>
+                <td>{user.createdAt}</td>
+                <td>{user.updatedAt}</td>
+                <td><img onClick={() => handleShowEdit(user.id)} src={edit_icon}/><img onClick={() => deleteUserById(user.id)} src={delete_icon}/></td>
+            </tr>
+            <Modal show={showEdit} onHide={handleCloseEdit} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add new user</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Full Name</Form.Label>
+                            <Form.Control onChange={fullNameOnChange} value={fullName}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control onChange={addressOnChange} value={address}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control onChange={phoneNumberOnChange} value={phoneNumber}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Category</Form.Label>
+                            <Form.Control onChange={categoryOnChange} value={category}/>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseEdit}>Close</Button>
+                    <Button onClick={handleSubmitEdit}>Save</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
 
